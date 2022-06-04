8202f56b56465cc1f0301d173598f64fccb9415f
AidaQ27 <aidaqueraltt@gmail.com>
2022-06-04 16:46:34 +0200

    segunda pantalla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   //Creem les varaibles
   title = 'UD34-Calculadora';
-  pantalla = "";
+  pantalla: any  = "";
   num1 = 0;
   num2 = 0;
   operador = "";
-  resultat = 0;
+  resultat: any = 0;
+  resultat2: any = 0;
+
   contador = 0;
+  operacioTotal: any = 0;
+  pantalla2: any = "";
 
 // Afegir numero a la pantalla
   printNumber(num: number) {
@@ -37,6 +41,39 @@ export class AppComponent {
 
   }
 
+  suma(){
+    this.operador = "+";
+    this.num1 = this.pantalla;  //Modificada la variable pantalla a any, para recoger el valor
+    this.pantalla = "";
+    this.contador = 0;
+  }
 
+  resta(){
+    this.operador = "-";
+    this.num1 = this.pantalla;  //Modificada la variable pantalla a any, para recoger el valor
+    this.pantalla = "";
+    this.contador = 0;
+  }
+
+  multi(){
+    this.operador = "*";
+    this.num1 = this.pantalla;  //Modificada la variable pantalla a any, para recoger el valor
+    this.pantalla = "";
+    this.contador = 0;
+
+  }
+
+
+  igual() {
+    this.num2 = this.pantalla;
+    this.resultat = eval(this.num1 + this.operador + this.num2);
+    this.pantalla = this.resultat;
+    this.resultat2 = (this.num1 + this.operador + this.num2);
+    this.pantalla2 = this.resultat2;
+
+
+
+  }
   // Reiniciar operadores a contador= 0
 }
+
